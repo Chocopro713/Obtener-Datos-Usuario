986c2ce054186d3f8a440cdd787351c698a1a4af
Anderson Barbosa <andersonbarbosadev@outlook.com>
2020-11-20 19:41:14 -0500

    Se mejora interfaz grafica

--- DatosUsuario/Services/Request/ApiManager.cs
+++ DatosUsuario/Services/Request/ApiManager.cs
@@ -83,7 +83,6 @@ namespace DatosUsuario.Services.Request
                 _userDialogs.Toast(strngResponse, TimeSpan.FromSeconds(3));
                 return data;
             }
-            
 
 
             data = await Policy
